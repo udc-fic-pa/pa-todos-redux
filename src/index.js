c0c00e58640913042c06f3a2ee78f36cec56a675
Fernando <fbellas@udc.es>
2021-03-26 21:00:55 +0100

    Actualización a create-react-app 4.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 
-import * as serviceWorker from './serviceWorker';
 import App from './App';
 import './styles.css';
 import reducer from './reducer';
@@ -20,5 +19,3 @@ ReactDOM.render(
         </Provider>
     </React.StrictMode>,
     document.getElementById('root'));
-
-serviceWorker.register();
