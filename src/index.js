71560e8d2eb77286d594d3b419b50e28fc462fe9
Fernando <fbellas@udc.es>
2022-06-15 10:35:36 +0200

    Actualización de librerías, incorporación de Redux Toolkit y migración a Node 16 LTS.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
+import ReactDOM from 'react-dom/client';
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
 
 import App from './App';
 import './styles.css';
 import reducer from './reducer';
 
 /* Configure store. */
-const store = createStore(reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = configureStore({reducer});
 
 /* Render application. */
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <App/>
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root'));
+    </React.StrictMode>);
